759be5ae81e01b977b52b6bbb049d7d99a8962e0
samarthchandaran264 <samarthchandarana35@gmail.com>
2026-01-23 16:49:00 +0530

    Add files via upload

---
+++ BestFriend.java
@@ -0,0 +1,6 @@
+public class BestFriend {
+    public static void main(String[] args) {
+        String bestFriendName = "Alex";
+        System.out.println("My best friend is " + bestFriendName);
+    }
+}
---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+
+        int product = a * b;
+        System.out.println("Product = " + product);
+    }
+}
